ed6e8513dc8213d6f72292396dc6199865ecf2fc
Merel <merel.steenbrink@gmail.com>
2020-03-05 12:22:35 +0100

    show list of items

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ItemList from "./item_list.js";
+
+
+class App extends Component {
+	render(){
+		return (
+			<div className= "container">
+				<ItemList/>
+			</div>
+			)
+	}
+}
+
+export default App;
---
+++ src/components/item_list.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+
+class ItemList extends Component {
+	constructor(props) {
+    	super(props);
+    	this.state = {
+      		error: null,
+      		isLoaded: false,
+      		items: []
+    	};
+  }
+
+
+  componentDidMount() {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then(res => res.json())
+      .then(result => {
+      	const promises = result
+      	.slice(0,30)
+      	.map(id => {
+      		return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+      		.then(response => response.json())
+      	})
+      	return Promise.all(promises)
+      })
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+      	<div className="item-list">
+	        <ol>
+	        	{this.state.items.map(item => {
+	        		return <li key={item.id}> {item.title} -- {item.by} </li>	
+	        	})}
+	        </ol>
+        </div>
+      );
+    }
+  }
+}
+
+
+export default ItemList;
