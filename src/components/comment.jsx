40a3c8d0eb2638cee9b0ec649228ce63fea42967
Merel <merel.steenbrink@gmail.com>
2020-03-07 18:25:54 +0100

    parse html of comments and temporarily show deleted comments

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -1,31 +1,32 @@
 import React, {Component} from 'react';
 import * as timeago from 'timeago.js';
+import parse from 'html-react-parser';
 
 class Comment extends Component {
 	renderChildren = (mum) => {
 		if (mum.kids) {
 			return ( <div className="comment-list">
-	        <ol>
-	        	{mum.kids.map(comment => {
-	        		return <Comment key={comment.id}
-	        		 comment={comment}
-	        		 />	
-	        	})}
-	        </ol>
-      </div> )
+				<ol>
+				{mum.kids.map(comment => {
+					return <Comment key={comment.id}
+					comment={comment}
+					/>
+				})}
+				</ol>
+				</div> )
 		}
 		else return
 	}
 
-	render(){
-		return (
-			<div className= "comment">
-				<div className="item-title"> {this.props.comment.by} -- {timeago.format(this.props.comment.time * 1000)}</div> 
-				<div className="text">{this.props.comment.text}</div>
-				<div className="children"> {this.renderChildren(this.props.comment)} </div>
-			</div>
-			)
-	}
+render(){
+	return (
+		<div className= "comment">
+			<div className="item-title"> {this.props.comment.by} -- {timeago.format(this.props.comment.time * 1000)}</div> 
+			<div className="text">{parse(this.props.comment.text || "DELETED")}</div>
+			<div className="children"> {this.renderChildren(this.props.comment)} </div>
+		</div>
+		)
+}
 }
 
 export default Comment;
